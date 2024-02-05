0dfd8781e38016f066624de4f60cb6aca8d76e6c
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-02-05 14:41:49 +0700

    Time: 138 ms (59.62%) | Memory: 18 MB (78.10%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums = sorted(nums)
-        occurance = 1
-        for i in range(len(nums)):
-            if nums[i] == nums[-i-1]:
-                return nums[i]
+        return nums[len(nums)//2]
             
                  
