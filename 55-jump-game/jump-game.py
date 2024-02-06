f9762eff5e7fb3eb1d6314731c4e313ef95f16c9
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-02-06 09:36:04 +0700

    Time: 347 ms (89.60%) | Memory: 17.9 MB (71.01%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,18 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        flag = 1
-        s = len(nums)
-        index_flag = s - 1
-        res = True
-        if s == 1:
-            return True
-        for i in range(s)[::-1]:
-            if nums[i] == 0 and flag == 1:
-                flag = 0
-                res = False
-                index_flag = i
-            elif  flag == 0:
-                if nums[i] > index_flag - i or nums[i] + i == s-1:
-                    res = True
-                    flag = 1
-        return res                
+        energy = 0
+        for num in nums:
+            if energy < 0:
+                return False
+            elif num > energy:
+                energy = num    
+            energy -= 1
+        return True      
