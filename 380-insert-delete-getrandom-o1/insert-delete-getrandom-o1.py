2e40cf7d403d30bd7b02cdfe378b843beb43d5bd
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-02-07 00:28:11 +0700

    Time: 1802 ms (5.00%) | Memory: 63.8 MB (87.24%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,32 +1,34 @@
 import random
-
 class RandomizedSet:
 
     def __init__(self):
-        self.lst = []
-        self.idx_map = {}
+        self.dict_ = defaultdict(int)
 
     def search(self, val):
-        return val in self.idx_map
-
-    def insert(self, val):
+        if val not in self.dict_:
+            return False
+        elif self.dict_[val] == 0:
+            return False
+        return True
+    def insert(self, val: int) -> bool:
         if self.search(val):
             return False
-
-        self.lst.append(val)
-        self.idx_map[val] = len(self.lst) - 1
+        self.dict_[val] += 1
         return True
 
-    def remove(self, val):
+    def remove(self, val: int) -> bool:
         if not self.search(val):
             return False
-
-        idx = self.idx_map[val]
-        self.lst[idx] = self.lst[-1]
-        self.idx_map[self.lst[-1]] = idx
-        self.lst.pop()
-        del self.idx_map[val]
+        self.dict_[val] -= 1
         return True
 
-    def getRandom(self):
-        return random.choice(self.lst)
+    def getRandom(self) -> int:
+        keys_list = [k for k, v in list(self.dict_.items()) if v != 0]
+        return random.choice(keys_list)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
