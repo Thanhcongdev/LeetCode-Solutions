cc8349d395903a80c784b26a983ec1666e839409
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-02-07 00:49:53 +0700

    Time: 251 ms (88.45%) | Memory: 63.2 MB (99.78%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,27 +1,32 @@
-import random
 class RandomizedSet:
 
     def __init__(self):
+        self.lst = []
         self.dict_ = {}
 
     def search(self, val):
-        if val not in self.dict_:
-            return False
-        return True
-    def insert(self, val: int) -> bool:
+        return val in self.dict_
+
+    def insert(self, val):
         if self.search(val):
             return False
-        self.dict_[val] = 1
+        self.dict_[val] = len(self.lst) 
+        self.lst.append(val)
         return True
 
-    def remove(self, val: int) -> bool:
+    def remove(self, val):
         if not self.search(val):
             return False
+
+        id_x = self.dict_[val]
+        self.lst[id_x] = self.lst[-1]
+        self.dict_[self.lst[-1]] = id_x
+        self.lst.pop()
         del self.dict_[val]
         return True
 
-    def getRandom(self) -> int:
-        return random.choice(list(self.dict_.keys()))
+    def getRandom(self):
+        return random.choice(self.lst)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
