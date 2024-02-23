82bc8681d1f8ea721646e56b0c410b62b35c2541
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-02-23 16:23:46 +0700

    Time: 561 ms (87.26%) | Memory: 21.1 MB (49.66%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,46 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
-        res = set()
-
-        #1. Split nums into three lists: negative numbers, positive numbers, and zeros
-        n, p, z = [], [], []
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0: 
-                n.append(num)
-            else:
-                z.append(num)
-
-        #2. Create a separate set for negatives and positives for O(1) look-up times
-        N, P = set(n), set(p)
-
-        #3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
-        #   i.e. (-3, 0, 3) = 0
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
-
-        #3. If there are at least 3 zeros in the list then also include (0, 0, 0) = 0
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        #4. For all pairs of negative numbers (-3, -1), check to see if their complement (4)
-        #   exists in the positive number set
-        for i in range(len(n)):
-            for j in range(i+1,len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-
-        #5. For all pairs of positive numbers (1, 1), check to see if their complement (-2)
-        #   exists in the negative number set
-        for i in range(len(p)):
-            for j in range(i+1,len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
-
-        return res     
+        ans = set()
+        neg, pos, zeros = [], [], []
+        for n in nums:
+            neg.append(n) if n < 0 else pos.append(n) if n > 0 else zeros.append(0)
+        # > 3 zero
+        if len(zeros) >= 3:
+            ans.add((0,0,0))
+        # at least 1 zero
+        set_n = set(neg)
+        set_p = set(pos)
+        if len(zeros) >= 1:      
+            for n in set_n:
+                if -n in set_p:
+                    ans.add((n, -n, 0))
+        # other
+        for i in range(len(neg)):
+            for j in range(i + 1, len(neg)):
+                target = -neg[i] - neg[j]
+                if target in set_p:
+                    ans.add(tuple(sorted((neg[i], neg[j], target))))
+        for i in range(len(pos)):
+            for j in range(i + 1, len(pos)):
+                target = -pos[i] - pos[j]
+                if target in set_n:
+                    ans.add(tuple(sorted((pos[i], pos[j], target))))
+        return ans
