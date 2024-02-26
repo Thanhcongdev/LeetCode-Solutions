496ccc7038d8592d6df8574a32a159434c5cb0ff
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-02-26 21:10:56 +0700

    Time: 44 ms (28.09%) | Memory: 16.6 MB (79.09%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -7,7 +7,6 @@ class Solution:
         for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        print(matrix)
         for i in range(n):
             for j in range(n//2):
                 matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
