7920095f21f1789efbb1ab131ae3a74e0babd2b7
Nguyễn Thành Công <58556683+Thanhcongdev@users.noreply.github.com>
2024-04-24 17:08:57 +0700

    Time: 85 ms (33.87%) | Memory: 16.9 MB (34.28%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        def findmeadian(merged):
+        def findMedian(merged):
             total = len(merged)
             if total % 2 == 1:
                 return float(merged[total // 2])
@@ -8,30 +8,29 @@ class Solution:
                 middle1 = merged[total // 2 - 1]
                 middle2 = merged[total // 2]
                 return (float(middle1) + float(middle2)) / 2.0
-        def compare_half(chunk1, chunk2, m, n):
-            print(chunk1, chunk2, m, n)
-            if m < 2 or n < 2:
-                merged = nums1 + nums2
-                merged.sort()
-                return findmeadian(merged)
-            i = m // 2
-            j = n // 2
-            mean1 = chunk1[i] if m % 2 == 0 else sum(chunk1[i:i+2]) / 2
-            mean2 = chunk2[j] if n % 2 == 0 else sum(chunk2[j:j+2]) / 2
-            if mean1 >= mean2:
-                if n % 2 != 0:
-                    j += 1
-                return compare_half(chunk1[:i+1], chunk2[j:], m // 2, n // 2)
+
+        m, n = len(nums1), len(nums2)
+        if m == 0:
+            return findMedian(nums2)
+        if n == 0:
+            return findMedian(nums1)
+
+        i, j = 0, 0
+        merged = []
+        while i < m and j < n:
+            if nums1[i] <= nums2[j]:
+                merged.append(nums1[i])
+                i += 1
             else:
-                if m % 2 != 0:
-                    i += 1
-                return compare_half(chunk1[i:], chunk2[:j+1], m // 2, n // 2)
+                merged.append(nums2[j])
+                j += 1
 
-        m, n = len(nums1) - 1, len(nums2) - 1
-        if m < 0:
-            return findmeadian(nums2)
-        if n < 0:
-            return findmeadian(nums1)
+        while i < m:
+            merged.append(nums1[i])
+            i += 1
 
-        num = compare_half(nums1, nums2, m, n)
-        return num
+        while j < n:
+            merged.append(nums2[j])
+            j += 1
+
+        return findMedian(merged)
